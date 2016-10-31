74d46820c9151057b6bd6bfa8ea4fead85cd03f0
Bianca Peixoto <bpeixoto18@gmail.com>
2016-10-31 20:02:37 -0200

    Update Capítulo 11 - Download.py

--- Capítulo 11 - Download.py
+++ Capítulo 11 - Download.py
@@ -2,11 +2,10 @@ import requests
 import os
 import bs4
 
-url = 'http://xkcd.com' #Starting url
-os.makedirs('xkcd', exist_ok=True)  #Store comics in ./xkcd
+url = 'http://xkcd.com' 
+os.makedirs('xkcd', exist_ok=True) 
 
 while not url.endswith('#'):
-    #Download the page
     print('Downloading page %s...' % url)
     res = requests.get(url)
     res.raise_for_status()
@@ -18,7 +17,6 @@ while not url.endswith('#'):
 
     else:
         comicUrl = 'http:' + comicElem[0].get('src')
-        # Download the image
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
         res.raise_for_status()
@@ -29,7 +27,6 @@ while not url.endswith('#'):
 
 
 
-    #Get the Prev button's url.
     prevLink = soup.select('a[rel="prev"]')[0]
     url = 'http://xkcd.com' + prevLink.get('href')
 
